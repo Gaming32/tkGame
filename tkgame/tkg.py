e2516876806cb02226e488e9829a0bc725d2c987
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-03-20 15:23:02 -0500

    3/19/2019

--- tkgame/tkg.py
+++ tkgame/tkg.py
@@ -3,8 +3,22 @@ import hashlib
 from modulefinder import ModuleFinder
 import sys
 import os
+from python_tools.path import bestrel
+printfaker = lambda *p, **kw: None
+
+if __name__ == '__main__':
+    trace = print
+else:
+    trace = printfaker
+
+if '-v' in sys.argv and trace != printfaker:
+    verbose = print
+else:
+    verbose = printfaker
+verbose = print
 
 def maketkg(meta):
+    trace('Initializing...')
     modfind = ModuleFinder()
     modfind.run_script(meta)
     try: gamename = modfind.modules['__main__'].NAME
@@ -14,22 +28,29 @@ def maketkg(meta):
     tkgpath = os.path.join(tkgpath, gamename + '.tkg')
     #if os.path.isfile(tkgpath): os.remove(tkgpath)
     #open(tkgpath, 'w+').close()
-    tkg = zipfile.ZipFile(tkgpath, 'w')
+    tkg = zipfile.ZipFile(tkgpath, 'w', zipfile.ZIP_DEFLATED)
+    hasher = hashlib.md5()
+    trace('Copying Modules...')
     for (name, mod) in modfind.modules.items():
         modfile = mod.__file__
         if not modfile: continue
         path = os.path.basename(modfile)
-        for fol in sys.path:
-            testpath = os.path.relpath(modfile, fol)
-            if not '..' in testpath:
-                path = testpath
-                break
-        print(name, '=>', path)
+        path = bestrel(modfile, os.path.dirname(modfind.modules['__main__'].__file__), *sys.path[1:])
+        # for fol in sys.path:
+        #     testpath = os.path.relpath(modfile, fol)
+        #     if not '..' in testpath:
+        #         path = testpath
+        #         break
+        verbose('\t', name, '=>', path)
         if name == '__main__':
             tkg.writestr('mainfile', path)
         #if '__pycache__' in modfile:
         #    path = os.path.join('__pycache__', path)
+        hasher.update(open(modfile, 'rb').read())
         tkg.write(modfile, path)
+    trace('Hashing...')
+    tkg.writestr('hash', hasher.digest())
+    trace('Done')
 
 if __name__ == '__main__':
     maketkg('Tanks/meta.py')
